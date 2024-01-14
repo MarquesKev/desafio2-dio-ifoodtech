89e7a969b0a8553a4fb6e78ded12a7b2b7ef5d11
MarquesKev <kevinlucas2702@gmail.com>
2024-01-14 20:33:31 -0300

    correções

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function player(victorys, defeats) {
-    return (victorys - defeats)
+function player(vitórias, derrotas) {
+    return (vitórias - derrotas)
 }
 
 let mmr = player(215, 125)
 
 function rank() {
     if (mmr <= 10){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Ferro")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Ferro.")
     } else if (mmr <= 20){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Bronze")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Bronze.")
     } else if (mmr <= 50){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Prata")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Prata.")
     } else if (mmr <= 80){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Ouro")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Ouro.")
     } else if (mmr <= 90){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Diamante")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Diamante.")
     } else if (mmr <= 100){
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Lendário")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Lendário.")
     } else {
-        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Imortal")
+        console.log ("O herói tem saldo de " + mmr + " vitórias, portanto está no nível Imortal.")
     }
 }
 
